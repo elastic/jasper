188c338a9394ffdb78789cd1cf1cb476a0bb57b7
Court Ewing <court@epixa.com>
2016-03-12 11:50:50 -0500

    [cmd] github limit

--- scripts/github.js
+++ scripts/github.js
@@ -1,5 +1,8 @@
 // Description:
 //   Set up a github client and make sure it is attached to the robot
+//
+// Commands:
+//   jasper github limit - Returns the current rate limiting info from github
 
 'use strict';
 
@@ -9,4 +12,11 @@ const github = octonode.client(token);
 
 module.exports = robot => {
   robot.github = github;
+
+  robot.respond(/github limit/, res => {
+    github.limit(function (err, left, max) {
+      if (err) return robot.emit('err', err);
+      res.send(`${left} of ${max} remaining`);
+    });
+  });
 };
