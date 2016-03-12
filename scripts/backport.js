3123c096e6642a8154c383e194f882cd2fd9509d
Court Ewing <court@epixa.com>
2016-03-12 11:52:07 -0500

    [backport] Rough outline of backporting steps

--- scripts/backport.js
+++ scripts/backport.js
@@ -2,29 +2,76 @@
 //   Backport a pull request to specific branches
 //
 // Commands:
-//   jasper backport <pr-url> <versions> - Backport the given PR to the space-separated versions
+//   jasper backport <pr-url> <branches> - Backport the given PR to the space-separated branches
 
 'use strict';
 
+const BACKPORT_REGEX = /backport (\S+) ((?:\S *)+)/;
+const PR_URL_REGEX = /^https\:\/\/github.com\/([^\/]+\/[^\/]+)\/pull\/(\d+)$/;
+
 module.exports = robot => {
-  robot.respond(/backport (\S+) ((?:\S *)+)/, res => {
+  robot.respond(BACKPORT_REGEX, res => {
+    const github = robot.github;
+
     const cmd = res.match.shift();
     const url = res.match.shift();
-    const versions = res.match.shift().split(/\s+/);
-    res.send("backport to", cmd, url, versions);
+    const branches = res.match.shift().split(/\s+/);
+    //res.send("backport to", cmd, url, branches);
 
-    // assert PR is merged
-    // assert target is not in versions
-    // for every version
-      // create backport-<pull>-<version>
-      // cherry-pick each commit, commit any conflicts
-      // issue PR to <version> branch, label:backport label:noconflicts label:v<version>
+    // get org/repo/pull# from url
+    const match = url.match(PR_URL_REGEX);
+    match.shift(); // get rid of first value
+    const repo = match.shift();
+    const number = match.shift();
+    console.log(repo, number);
+
+    const pr = github.pr(repo, number);
+
+    pr.info((err, info) => {
+      if (err) return console.error('err', err);
+      //console.log(info);
+
+      const target = info.base.ref;
+      if (branches.indexOf(target) > -1) return console.error('cannot backport into original pr target branch');
+
+      const merged = info.merged;
+      if (!merged) return console.error('pr is not yet merged'); // todo: uncomment
+
+      // todo: do this in parallel
+      pr.commits((err, commits) => {
+        if (err) return console.error('err', err);
+
+        const backports = branches.map(version => {
+          // assert that we have appropriate labels
+
+          // create backport-<pull>-<version>
+          const branch = `jasper-backport-${number}-${version}`;
+          console.log(`create ${branch}`);
+
+          // cherry-pick each commit, commit any conflicts(?)
+          commits.forEach(commit => {
+            console.log(`cherry-pick ${commit.sha}`);
+          });
+
+          // issue PR to <version> branch, label:backport label:noconflicts
+          const labels = ['backport', 'noconflicts'];
+          console.log(`open pr to ${version} from ${branch} with labels: ${labels.join()}`);
+
+          return branch;
+        });
+
+        // push changes to upstream
+        console.log(`push branches to upstream: ${backports.join()}`);
+
+        res.send('done');
+      })
+    });
   });
 
   // whenever backport PR without conflicts/updates goes green, merge
   robot.router.post('/backport', (req, res) => {
     // do nothing if PR is not in exactly this state:
-      // labels: backport, noconflicts, v<version>
+      // labels: backport, noconflicts
       // green build, green cla
       // original commits?
   });
