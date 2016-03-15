339e43cb5d927a5024fcf780af6d637f1ccc6470
Court Ewing <court@epixa.com>
2016-03-15 10:15:09 -0400

    Use bluebird for promisification
    Native promises are great, but bluebird provides convenient utilities
    for automatically converting functions that invoke node style callbacks
    into functions that return promises.

--- scripts/backport.js
+++ scripts/backport.js
@@ -7,6 +7,7 @@
 'use strict';
 
 const resolve = require('path').resolve;
+const promisify = require('bluebird').promisify;
 const includes = require('lodash').includes;
 const mkdirp = require('mkdirp');
 const nodegit = require('nodegit');
@@ -32,56 +33,53 @@ module.exports = robot => {
 
     const pr = github.pr(repo, number);
 
-    pr.info((err, info) => {
-      if (err) return console.error('err', err);
-      //console.log(info);
+    promisify(pr.info.bind(pr))()
+      .then(info => {
+        const target = info.base.ref;
+        if (includes(branches, target)) return console.error('cannot backport into original pr target branch');
 
-      const target = info.base.ref;
-      if (includes(branches, target)) return console.error('cannot backport into original pr target branch');
+        const merged = info.merged;
+        if (!merged) return console.error('pr is not yet merged'); // todo: uncomment
 
-      const merged = info.merged;
-      if (!merged) return console.error('pr is not yet merged'); // todo: uncomment
+        const repoDir = resolve(__dirname, '..', 'repos', repo);
+        mkdirp.sync(repoDir); // todo: unsync this
+        return nodegit.Clone(info.base.repo.clone_url, repoDir)
+          .catch(err => {
+            if (!includes(err.message, 'exists and is not an empty directory')) {
+              throw err;
+            }
+            return nodegit.Repository.open(repoDir);
+          })
+          .then(repo => {
+            // todo: do this in parallel
+            return promisify(pr.commits.bind(pr))().then(commits => {
+              const backports = branches.map(version => {
+                // assert that we have appropriate labels
 
-      const repoDir = resolve(__dirname, '..', 'repos', repo);
-      mkdirp.sync(repoDir); // todo: unsync this
-      nodegit.Clone(info.base.repo.clone_url, repoDir)
-        .catch(err => {
-          if (!includes(err.message, 'exists and is not an empty directory')) {
-            return robot.emit('error', err);
-          }
-          return nodegit.Repository.open(repoDir);
-        })
-        .then(repo => {
-          // todo: do this in parallel
-          pr.commits((err, commits) => {
-            if (err) return console.error('err', err);
+                // create backport-<pull>-<version>
+                const branch = `jasper-backport-${number}-${version}`;
+                console.log(`create ${branch}`);
 
-            const backports = branches.map(version => {
-              // assert that we have appropriate labels
+                // cherry-pick each commit, commit any conflicts(?)
+                commits.forEach(commit => {
+                  console.log(`cherry-pick ${commit.sha}`);
+                });
 
-              // create backport-<pull>-<version>
-              const branch = `jasper-backport-${number}-${version}`;
-              console.log(`create ${branch}`);
+                // issue PR to <version> branch, label:backport label:noconflicts
+                const labels = ['backport', 'noconflicts'];
+                console.log(`open pr to ${version} from ${branch} with labels: ${labels.join()}`);
 
-              // cherry-pick each commit, commit any conflicts(?)
-              commits.forEach(commit => {
-                console.log(`cherry-pick ${commit.sha}`);
+                return branch;
               });
 
-              // issue PR to <version> branch, label:backport label:noconflicts
-              const labels = ['backport', 'noconflicts'];
-              console.log(`open pr to ${version} from ${branch} with labels: ${labels.join()}`);
+              // push changes to upstream
+              console.log(`push branches to upstream: ${backports.join()}`);
 
-              return branch;
+              res.send('done');
             });
-
-            // push changes to upstream
-            console.log(`push branches to upstream: ${backports.join()}`);
-
-            res.send('done');
-          })
-        });
-    });
+          });
+      })
+      .catch(err => robot.emit('error', err));
   });
 
   // whenever backport PR without conflicts/updates goes green, merge
--- scripts/github.js
+++ scripts/github.js
@@ -6,17 +6,17 @@
 
 'use strict';
 
+const promisifyAll = require('bluebird').promisifyAll;
 const octonode = require('octonode');
 const token = require('../.github-access.json').token;
 const github = octonode.client(token);
 
 module.exports = robot => {
-  robot.github = github;
+  robot.github = promisifyAll(github);
 
   robot.respond(/github limit/, res => {
-    github.limit(function (err, left, max) {
-      if (err) return robot.emit('err', err);
-      res.send(`${left} of ${max} remaining`);
-    });
+    github.limitAsync()
+      .then(left => res.send(`${left} requests remaining`))
+      .catch(err => robot.emit('error', err));
   });
 };
