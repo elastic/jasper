1738d8df3dd43bb25922d23df0736318b42faaf2
Court Ewing <court@epixa.com>
2016-03-15 11:36:27 -0400

    Use node --harmony
    Destructure to your heart's content.
    
    In order to enable harmony flags for node.js while still being able to
    run hubot (which requires coffeescript) without deprecation warnings, we
    need to make sure we're using a more recent version of coffeescript than
    is bundled with hubot itself.

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,3 +1,5 @@
 @echo off
 
-node_modules\.bin\hubot.cmd --name "jasper" %*
+node_modules/.bin/coffee.cmd^
+ --nodejs "--harmony --harmony_destructuring --harmony_default_parameters"^
+ node_modules/.bin/hubot.cmd --name "jasper" %*
