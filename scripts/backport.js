afb4a72c253f4a94dd079ac7e7ca5e2d1c3d9d4f
Court Ewing <court@epixa.com>
2016-03-20 10:06:59 -0400

    [backport] Write diff to tmp file

--- scripts/backport.js
+++ scripts/backport.js
@@ -6,10 +6,13 @@
 
 'use strict';
 
+const fs = require('fs');
+
 const { includes } = require('lodash');
 const { resolve } = require('path');
 const { promisify } = require('bluebird');
 const request = require('request');
+const tmp = require('tmp');
 
 const { getCommits, getInfo } = require('../src/github');
 const { openOrClone } = require('../src/git');
@@ -18,6 +21,8 @@ const BACKPORT_REGEX = /backport (\S+) ((?:\S *)+)/;
 const PR_URL_REGEX = /^https\:\/\/github.com\/([^\/]+\/[^\/]+)\/pull\/(\d+)$/;
 
 const requestGet = promisify(request.get);
+const tmpFile = promisify(tmp.file, { multiArgs: true });
+const writeFile = promisify(fs.writeFile);
 
 module.exports = robot => {
   robot.respond(BACKPORT_REGEX, res => {
@@ -65,7 +70,13 @@ module.exports = robot => {
         return msg.join('\n'); // between lines of an individual message
       }).join('\n\n'); // between messages
 
-      const loadDiff = requestGet(info.diff_url).then(res => res.body);
+      const diffFile = requestGet(info.diff_url)
+        .then(res => res.body)
+        .then(diff => {
+          return tmpFile({ prefix: 'jasper-' }).then(([ path, fd, cleanup ]) => {
+            return writeFile(path, diff).then(() => [ path, cleanup ]);
+          });
+        });
 
       const repoDir = resolve(__dirname, '..', 'repos', repo);
       return openOrClone(repoDir, info.base.repo.clone_url).then(repo => {
@@ -79,12 +90,13 @@ module.exports = robot => {
               .then(() => repo.getReferenceCommit(`origin/${target}`))
               .then(commit => repo.createBranch(backportBranch, commit))
               .then(() => repo.checkoutBranch(backportBranch))
-              .then(() => loadDiff)
-              .then(diff => {
+              .then(() => diffFile)
+              .then(([ path, cleanup ]) => {
                 console.log(`\n${commitMessage}`);
-                console.log(`\n\n${diff}`);
+                console.log(`\n\n${path}`);
 
                 // todo: issue PR to <target> branch, label:backport label:noconflicts
+                cleanup();
               });
           }, repo.fetch('origin'))
           .then(() => {
